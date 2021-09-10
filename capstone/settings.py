60c0097c0e044fa106369a6ec5d8f9db28b8bfdd
Petals <ptsnoir@gmail.com>
2021-09-10 16:07:39 +0800

    updated settings

--- capstone/settings.py
+++ capstone/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wj-jr%bi(y52jwzagqzuq07$2$fhz&t+6u6_5(7t&--wys-8+u'
+# SECRET_KEY = 'wj-jr%bi(y52jwzagqzuq07$2$fhz&t+6u6_5(7t&--wys-8+u'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['plantsmemo.herokuapp.com']
 
 
 # Application definition
